09c403972a04473c304b703c51ab85e2287c46cc
ashwanizsktm <48983045+ashwanizsktm@users.noreply.github.com>
2023-09-17 17:10:05 +0530

    classes and inheritence

--- index.ts
+++ index.ts
@@ -1,9 +1,4 @@
-// Import stylesheets
-import './style.css';
-
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+/*========= TYPESCRIPT =========== */
 
 // types
 
@@ -123,7 +118,8 @@ console.log(airplane);
 
 // union
 
-const Number: string | number = 23;
+//  const Number: string | number = 23;
+
 // we can either put any of the type here
 /*
  usecase:- let's suppose we are making an api call & we know the type of errors are going to be string but we don't know whether we get an error or not so in that case we'll put the initial value as null see the below example..
@@ -131,7 +127,10 @@ const Number: string | number = 23;
 
 const Errors: string | null = null;
 
-// remember never use something like this, this not the recemmended way or not supporable.
+/*
+ remember never use something like this, this not the recemmended way or not supporable.
+ */
+
 const SomeProps: string | null | object | string[] | undefined = null;
 
 // aliases
@@ -189,6 +188,220 @@ if we try to assign anything in the someProps except undefined & null it'll thro
  any is the worst type in typescript it does't let you what type of data is going to be.
 */
 
-//it can only be usefull in such a case where we no idea at all what type
+// it can only be usefull in such a case where we no idea at all what type
 
-const someProps: any = 'hello'; // in the some props we can assing the value of any type, string, number, boolean array object and whatever can be.
+// const someProps: any = 'hello';
+
+// in the some props we can assing the value of any type, string, number, boolean array object and whatever can be.
+
+// never
+/*
+ this mean that the something will never happen it is used in the such cases
+ as you see from the example.
+ */
+
+/*
+  const doSomeThing = ():never => {
+    throw 'Error'
+  }
+*/
+
+/*
+ unknown it is an alternative to any but it's naming is so clear that we don't know what kind of type we are going to put inside;
+*/
+
+const someProps: unknown = 24;
+
+// let's see the other example to see the difference between any and unknown.
+
+/*
+
+let vAny: any = 10;
+let vUnknown: unknown = 10;
+
+let s1: string = vAny; // no error
+let s2: string = vUnknown; //Type 'unknown' is not assignable to type 'string'.(2322)
+console.log(s1);
+
+console.log(vAny.foo()) // it'll not throw an error
+console.log(vUnknown.foo()) // Property 'foo' does not exist on type 'unknown'.(2339)
+
+*/
+
+// Type Assertion/typecasting : it actually means converting one data type to other
+/*
+let vUnknown: unknown = 10;
+
+// let s2: number = vUnknown;
+
+// error Type 'unknown' is not assignable to type 'number'.(2322)
+// so to fix this we use type Assertion
+
+let s2: number = vUnknown as number;
+
+console.log(s2);
+*/
+
+// lets see a another useful case
+
+//let pageNumber: string = '10';
+
+//let pageNumericNumber: number = pageNumber as number;
+
+// this will also throw an error in the editor
+/*
+  Conversion of type 'string' to type 'number' may be a mistake because neither type sufficiently overlaps with the other. If this was intentional, convert the expression to 'unknown' first.(2352)
+*/
+
+// so the above line cnn be written as something like this.
+
+/*
+let pageNumericNumber: number = pageNumber as unknown as number;
+console.log(pageNumericNumber);
+*/
+
+// typescript with DOM here the typeassertion/ typecasting plays an important role.
+// typescript doesn't know anything about our markup
+
+/*
+const inputBox = document.querySelector('.input-box');
+console.log(inputBox.value) // we get error so to fix that we use typecast
+*/
+
+/*
+const inputBox = document.querySelector('.input-box') as HTMLInputElement;
+// console.log(inputBox.value);
+
+// adding listeners to it.
+
+inputBox.addEventListener('mouseup', (event: Event) => {
+  // console.log(event.target.value) // this line won't read the value on target so
+  // that we use typecast here
+
+  const target = event.target as HTMLInputElement;
+  console.log(target.value);
+});
+*/
+
+// tyscript classes
+/*
+class User {
+  private firstName: string;
+  private lastname: string;
+  readonly unchangableName: string;
+  constructor(fname: string, lname: string) {
+    this.firstName = fname;
+    this.lastname = lname;
+    this.unchangableName = fname;
+  }
+
+  getUnchangableName(): void {
+    // this.unchangableName = 'Foo';
+    // Cannot assign to 'unchangableName' because it is a read-only property.(2540)
+  }
+
+  getFullName(): string {
+    return this.firstName + ' ' + this.lastname;
+  }
+}
+
+  const user: User = new User('Ashwani', 'Kumar');
+
+  console.log(user);
+  console.log(user.getFullName());
+
+*/
+// console.log(user.firstName)
+//Property 'firstName' is private and only accessible within class 'User'
+
+/*
+ * By default every property of a class is public
+ * by wring private will not let me access that.
+ * protected we use with interfaces
+ */
+
+// classes with interfaces
+
+/*
+interface UserInterface {
+  getFullName(): string;
+}
+
+class User implements UserInterface {
+  firstName: string;
+  lastname: string;
+  static readonly maxAge = 25;
+  // by writing static keyword before readony it'll be accessble outside of the class.
+
+  constructor(fname: string, lname: string) {
+    this.firstName = fname;
+    this.lastname = lname;
+  }
+
+  getFullName(): string {
+    return this.firstName + ' ' + this.lastname;
+  }
+}
+
+// console.log(User.maxAge);
+
+// let's inherit the user class
+class Admin extends User {
+  private editor: string;
+
+  setEditor(editor: string): void {
+    this.editor = editor;
+  }
+
+  getEditor(): string {
+    return this.editor;
+  }
+}
+
+// 1. without using constructor inside the Admin class it'll also have all the
+// properties of a class.
+
+const admin = new Admin('Ashwani', 'kumar');
+
+console.log(admin.firstName);
+console.log(admin.lastname);
+admin.setEditor(`FullName: ${admin.firstName} ${admin.lastname}`);
+console.log(admin.getEditor());
+*/
+
+// example 2. with proper constructor instantiation
+/*
+// parent class
+class User {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age; // if age property with types is not present in class there'lll be type error.
+  }
+}
+
+// student class
+class Student extends User {
+  private marks: number;
+  constructor(name: string, age: number, marks: number) {
+    super(name, age); // by calling super() we are accessing the name and age props of parent class. it'll throw an error once we don't use super class
+    this.marks = marks;
+  }
+}
+
+const student: Student = new Student('Rahul', 25, 255);
+console.log(student);
+
+// teacher class
+class Teacher extends Student {
+  private classSize: number;
+  constructor(name: string, age: number, marks: number, classSize: number) {
+    super(name, age, marks);
+    this.classSize = classSize;
+  }
+}
+
+const teacher: Teacher = new Teacher('Mohan', 45, 500, 13);
+console.log(teacher);
+*/
